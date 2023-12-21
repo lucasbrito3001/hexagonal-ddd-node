434170fd8866628fd697f21cec6c54cf6ef36da0
lucasbrito3001 <lucas.brito3001@gmail.com>
2023-12-20 22:30:47 -0300

    fix: Change type of params in BookRepositoryPort

--- src/resources/book/persistence/port/BookRepositoryPort.ts
+++ src/resources/book/persistence/port/BookRepositoryPort.ts
@@ -1,10 +1,9 @@
-import { StockBookDTO } from "../../controller/dto/StockBookDto";
 import { Book } from "../../domain/Book";
 import { BookEntity } from "../BookEntity";
 
 export interface BookRepositoryPort {
-	save(stockBook: Book): Promise<void>;
-	update(id: string, stockBook: Book): Promise<void>;
+	save(book: Book): Promise<void>;
+	update(id: string, book: Book): Promise<void>;
 	search(title: string): Promise<BookEntity[]>;
 	get(id: string): Promise<BookEntity | null>;
 	searchByIds(ids: string[]): Promise<BookEntity[]>;
